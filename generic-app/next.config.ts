123b3a8fb51ae7f49ebce5634f0e1449808910dc
IagoGrangeiro <90574669+IagoGrangeiro@users.noreply.github.com>
2024-12-08 05:21:47 +0000

    X-forwarded-host-header

--- generic-app/next.config.ts
+++ generic-app/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+		serverActions: {
+			allowedOrigins: [
+        'localhost:3000',
+        "automatic-space-rotary-phone-pq6rwvvxw5pf7wr6-3000.app.github.dev"
+      ]
+		},
+	}
 };
 
 export default nextConfig;
